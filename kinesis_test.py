98b6007eb60eedce5cd52b9f1b9ec9c8e06fec60
msaad <msaad@shift-ev.com>
2022-09-29 16:11:13 +0200

    first realease

---
+++ kinesis_test.py
@@ -0,0 +1,27 @@
+import datetime
+import json
+import random
+import boto3
+
+STREAM_NAME = "ExampleInputStream"
+
+
+def get_data():
+    return {
+        'EVENT_TIME': datetime.datetime.now().isoformat(),
+        'TICKER': random.choice(['AAPL', 'AMZN', 'MSFT', 'INTC', 'TBV']),
+        'PRICE': round(random.random() * 100, 2)}
+
+
+def generate(stream_name, kinesis_client):
+    while True:
+        data = get_data()
+        print(data)
+        kinesis_client.put_record(
+            StreamName=stream_name,
+            Data=json.dumps(data),
+            PartitionKey="partitionkey")
+
+
+if __name__ == '__main__':
+    generate(STREAM_NAME, boto3.client('kinesis'))
